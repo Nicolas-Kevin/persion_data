08d8630a867679978b0457b0c65e5b90837f2c70
yuanwenkai <yuanwenkai@v-secure.cn>
2022-05-29 23:14:20 +0800

    后端增加添加人员校验

--- internal/api/personnel/func_create.go
+++ internal/api/personnel/func_create.go
@@ -1,34 +1,35 @@
 package personnel
 
 import (
-	"net/http"
-
+	"github.com/go-playground/validator/v10"
 	"go-gin-api/internal/code"
 	"go-gin-api/internal/pkg/core"
 	"go-gin-api/internal/services/personnel"
+	"net/http"
+	"regexp"
 )
 
 type createRequest struct {
-	UserName         string `form:"UserName"`         // 姓名
-	UserAge          int32  `form:"UserAge"`          // 年龄
-	UserSex          int32  `form:"UserSex"`          // 性别（0：女，1：男）
-	UserDate         string `form:"UserDate"`         // 出生年月日
-	PoliticalOutLook int32  `form:"PoliticalOutLook"` // 社会面貌：0 :普通公民，1：团员，2：党员
-	Company          string `form:"Company"`          // 工作单位
-	Position         string `form:"Position"`         // 职称/等级
-	Major            string `form:"Major"`            // 专业
-	UserLevel        string `form:"UserLevel"`        // 级别
-	UserHeight       int32  `form:"UserHeight"`       // 身高cm
-	UserWeight       int32  `form:"UserWeight"`       // 体重cm
-	ShoeSize         int32  `form:"ShoeSize"`         // 鞋号cm
-	HeadNum          int32  `form:"HeadNum"`          // 头围cm
-	BloodType        string `form:"BloodType"`        // 血型cm
-	IdNum            string `form:"IdNum"`            // 身份证号
-	BankNum          string `form:"BankNum"`          // 银行卡号
-	PhoneNum         string `form:"PhoneNum"`         // 电话号
-	UserTeam         int64  `form:"UserTeam"`         // 部队编号
-	TrainStatus      string `form:"TrainStatus"`      // 演训情况
-	RewardStatus     string `form:"RewardStatus"`     // 奖励情况
+	UserName         string `form:"UserName" validator:"gt=0"`                // 姓名
+	UserAge          int32  `form:"UserAge" validator:"gt=0"`                 // 年龄
+	UserSex          int32  `form:"UserSex" validator:"oneof=0 1"`            // 性别（0：女，1：男）
+	UserDate         string `form:"UserDate" validator:"gt=0"`                // 出生年月日
+	PoliticalOutLook int32  `form:"PoliticalOutLook" validator:"oneof=0 1 2"` // 社会面貌：0 :普通公民，1：团员，2：党员
+	Company          string `form:"Company" validator:"gt=0"`                 // 工作单位
+	Position         string `form:"Position" `                                // 职称/等级
+	Major            string `form:"Major" `                                   // 专业
+	UserLevel        string `form:"UserLevel" validator:"gt=0"`               // 级别
+	UserHeight       int32  `form:"UserHeight" validator:"gt=0"`              // 身高cm
+	UserWeight       int32  `form:"UserWeight" validator:"gt=0"`              // 体重cm
+	ShoeSize         int32  `form:"ShoeSize" validator:"gt=0"`                // 鞋号cm
+	HeadNum          int32  `form:"HeadNum" validator:"gt=0"`                 // 头围cm
+	BloodType        string `form:"BloodType" validator:"gt=0"`               // 血型cm
+	IdNum            string `form:"IdNum" validator:"gt=0,len=18"`            // 身份证号
+	BankNum          string `form:"BankNum" validator:"gt=0"`                 // 银行卡号
+	PhoneNum         string `form:"PhoneNum" validator:"gt=0,len=11"`         // 电话号
+	UserTeam         int64  `form:"UserTeam"`                                 // 部队编号
+	TrainStatus      string `form:"TrainStatus" validator:"gt=0"`             // 演训情况
+	RewardStatus     string `form:"RewardStatus" validator:"gt=0"`            // 奖励情况
 
 }
 
@@ -52,6 +53,7 @@ func (h *handler) Create() core.HandlerFunc {
 	return func(ctx core.Context) {
 		req := new(createRequest)
 		res := new(createResponse)
+		validate := validator.New()
 		if err := ctx.ShouldBindForm(req); err != nil {
 			ctx.AbortWithError(core.Error(
 				http.StatusBadRequest,
@@ -61,6 +63,8 @@ func (h *handler) Create() core.HandlerFunc {
 			return
 		}
 		createData := new(personnel.CreatePersonnelData)
+		validate.RegisterStructValidation(idcardValidatng, createData)
+		validate.RegisterStructValidation(phoneNumValidatng, createData)
 		createData.UserName = req.UserName
 		createData.UserAge = req.UserAge
 		createData.UserSex = req.UserSex
@@ -81,6 +85,17 @@ func (h *handler) Create() core.HandlerFunc {
 		createData.UserTeam = req.UserTeam
 		createData.TrainStatus = req.TrainStatus
 		createData.RewardStatus = req.RewardStatus
+
+		//参数校验
+		err2 := validate.Struct(createData)
+		if err2 != nil {
+			ctx.AbortWithError(core.Error(
+				http.StatusBadRequest,
+				code.AuthorizedCreateError,
+				code.Text(code.AuthorizedCreateError)).WithError(err2),
+			)
+			return
+		}
 		id, err := h.personnelServcie.Create(ctx, createData)
 		if err != nil {
 			ctx.AbortWithError(core.Error(
@@ -95,4 +110,30 @@ func (h *handler) Create() core.HandlerFunc {
 		res.Msg = "添加成功"
 		ctx.Payload(res)
 	}
+}
+
+func phoneNumValidatng(sl validator.StructLevel) {
+	personnelData := sl.Current().Interface().(*personnel.CreatePersonnelData)
+	if len(personnelData.PhoneNum) == 11 {
+		matchString, err := regexp.MatchString("^1[3456789]\\d{9}$", personnelData.IdNum)
+		if err != nil || !matchString {
+			sl.ReportError("PhoneNum", "PhoneNum", "PhoneNum", "手机号格式错误", "")
+		}
+	} else {
+		sl.ReportError("PhoneNum", "PhoneNum", "PhoneNum", "手机号格式错误", "")
+	}
+}
+
+//身份正校验
+func idcardValidatng(sl validator.StructLevel) {
+	personnelData := sl.Current().Interface().(*personnel.CreatePersonnelData)
+	if len(personnelData.IdNum) == 18 {
+		matchString, err := regexp.MatchString("^([1-6][1-9]|50)\\d{4}(18|19|20)\\d{2}((0[1-9])|10|11|12)(([0-2][1-9])|10|20|30|31)\\d{3}[0-9Xx]$", personnelData.IdNum)
+		if err != nil || !matchString {
+			sl.ReportError("IdNum", "IdNum", "IdNum", "身份证格式错误", "")
+		}
+	} else {
+		sl.ReportError("IdNum", "IdNum", "IdNum", "身份证格式错误", "")
+	}
+
 }
