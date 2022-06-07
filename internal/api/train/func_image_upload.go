626522302f33068faef01a5cb37fa132b78fb821
yuanwenkai <yuanwenkai@v-secure.cn>
2022-06-07 20:59:34 +0800

    文件上传

--- internal/api/train/func_image_upload.go
+++ internal/api/train/func_image_upload.go
@@ -11,7 +11,8 @@ type imageUploadRequest struct {
 }
 
 type imageUploadResponse struct {
-	errCode int `json:"err_code"`
+	ErrCode int    `json:"err_code"`
+	Msg     string `json:"msg"`
 }
 
 // ImageUpload 文件上传
@@ -29,37 +30,10 @@ func (h *handler) ImageUpload() core.HandlerFunc {
 	return func(ctx core.Context) {
 		req := new(imageUploadResponse)
 		ctx.Request().ParseMultipartForm(ctx.Request().ContentLength)
-		//file, he, err := ctx.Request().FormFile("file")
-		//if err != nil {
-		//	fmt.Println(err.Error())
-		//	return
-		//}
-		//filename := he.Filename
-		//open, err := he.Open()
-		//var c []byte
-		//at, err := open.ReadAt(c, he.Size)
-		//if err != nil && err.Error() != "EOF" {
-		//	fmt.Println(err.Error(), at)
-		//	return
-		//}
-		//
-		//f2, err := os.Open(filename + ".png")
-		//f2.Write(c)
-		//defer f2.Close()
-		//f := []byte{}
-		//fmt.Println(file.Read(f))
-		// 解析出文件名，获取最后一个元素信息：A20_Apex.apk
-
-		//body, _ := ioutil.ReadAll(ctx.Request().Body)
-		//
-		//fmt.Println("---body/--- \r\n " + string(body))
-		//mm := make(map[string]interface{})
-		//json.Unmarshal(body, &mm)
-		//fmt.Println(mm)
 		headers := ctx.Request().MultipartForm.File["file"]
-		fmt.Println(headers)
+		filePaht := ""
 		for _, fheader := range headers {
-			err := h.trainService.ImageUpload(fheader)
+			path, err := h.trainService.ImageUpload(fheader)
 			if err != nil {
 				fmt.Println(err)
 				ctx.AbortWithError(core.Error(
@@ -67,10 +41,14 @@ func (h *handler) ImageUpload() core.HandlerFunc {
 					code.AuthorizedCreateError,
 					code.Text(code.AuthorizedCreateError)).WithError(err),
 				)
+
 				return
 			}
+			filePaht += path + ","
 		}
-		req.errCode = 200
-		ctx.Payload(nil)
+		req.ErrCode = http.StatusOK
+		req.Msg = filePaht
+		ctx.Payload(req)
+
 	}
 }
